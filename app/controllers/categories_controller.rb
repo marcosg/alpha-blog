03f45a29b3af4147183dfd716031a04fa8960517
marcosg <marcosg101@gmail.com>
2019-04-10 10:05:25 -0400

    Update all views with categories.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -19,7 +19,10 @@ class CategoriesController < ApplicationController
     end
   end
 
-  def show; end
+  def show
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
+  end
 
   private
 
